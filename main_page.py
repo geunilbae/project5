c58c32a0f258ae38a07e1b0f3c795fbcbb7417e3
ksy277 <tkddud4514@gmail.com>
2024-10-18 14:27:04 +0900

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -1,12 +1,14 @@
 import streamlit as st
-import webbrowser
 
-# 사이드바에서 선택할 수 있는 옵션들
-page = st.sidebar.selectbox(
-    'Go to website',
-    ['Select an option', 'Streamlit']
-)
+# 사이드바에서 선택 상자 생성
+page = st.sidebar.selectbox('Navigate', ['Select an option', 'Go to Site 1', 'Go to Site 2'])
 
-# 선택한 옵션에 따라 다른 URL로 이동
-if page == 'Streamlit':
-    webbrowser.open_new_tab('https://cheokcheok101py-3dfsncgsxm7qqz8tmaqt8y.streamlit.app/')
+# 첫 번째 메뉴 선택시 링크 제공
+if page == 'Go to Site 1':
+    st.write("You will be redirected to Site 1.")
+    st.markdown('<a href="https://cheokcheok101py-3dfsncgsxm7qqz8tmaqt8y.streamlit.app/" target="_self">Click here to go to Site 1</a>', unsafe_allow_html=True)
+
+# 두 번째 메뉴 선택시 링크 제공
+elif page == 'Go to Site 2':
+    st.write("You will be redirected to Site 2.")
+    st.markdown('<a href="https://cheokcheok102py-ubq3swbcmet64wqnmtzvhp.streamlit.app/" target="_self">Click here to go to Site 2</a>', unsafe_allow_html=True)
