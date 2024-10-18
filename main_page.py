33607fdf1824a82c212648d68fe62c72f0d71411
ksy277 <tkddud4514@gmail.com>
2024-10-18 14:27:58 +0900

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -3,12 +3,12 @@ import streamlit as st
 # 사이드바에서 선택 상자 생성
 page = st.sidebar.selectbox('Navigate', ['Select an option', 'Go to Site 1', 'Go to Site 2'])
 
-# 첫 번째 메뉴 선택시 링크 제공
+# 첫 번째 메뉴 선택시 Site 1로 리디렉션
 if page == 'Go to Site 1':
-    st.write("You will be redirected to Site 1.")
-    st.markdown('<a href="https://cheokcheok101py-3dfsncgsxm7qqz8tmaqt8y.streamlit.app/" target="_self">Click here to go to Site 1</a>', unsafe_allow_html=True)
+    js = "window.location.href = 'https://cheokcheok101py-3dfsncgsxm7qqz8tmaqt8y.streamlit.app/'"
+    st.components.v1.html(f"<script>{js}</script>", height=0)
 
-# 두 번째 메뉴 선택시 링크 제공
+# 두 번째 메뉴 선택시 Site 2로 리디렉션
 elif page == 'Go to Site 2':
-    st.write("You will be redirected to Site 2.")
-    st.markdown('<a href="https://cheokcheok102py-ubq3swbcmet64wqnmtzvhp.streamlit.app/" target="_self">Click here to go to Site 2</a>', unsafe_allow_html=True)
+    js = "window.location.href = 'https://cheokcheok102py-ubq3swbcmet64wqnmtzvhp.streamlit.app/'"
+    st.components.v1.html(f"<script>{js}</script>", height=0)
