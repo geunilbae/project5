1e529a22300d8f996ed73a1aae31f0a986ae2830
ksy277 <tkddud4514@gmail.com>
2024-10-18 14:22:44 +0900

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -1,11 +1,12 @@
 import streamlit as st
-import importlib
+import webbrowser
 
-# 사이드바에서 페이지 선택
-page = st.sidebar.selectbox('Navigate', ['Page 1'])
+# 사이드바에서 선택할 수 있는 옵션들
+page = st.sidebar.selectbox(
+    'Go to website',
+    ['Select an option', 'Streamlit']
+)
 
-# 홈 페이지 처리
-if page == 'Page 1':
-    st.title("Page 1")
-    page1 = importlib.import_module('cheokcheok1_01')
-    page1.show()
+# 선택한 옵션에 따라 다른 URL로 이동
+if page == 'Streamlit':
+    webbrowser.open_new_tab('https://cheokcheok101py-3dfsncgsxm7qqz8tmaqt8y.streamlit.app/')
