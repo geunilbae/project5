97247b337b3c246d17c25dede5f840c0752e1c6c
ksy277 <tkddud4514@gmail.com>
2024-10-18 14:29:24 +0900

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -5,10 +5,10 @@ page = st.sidebar.selectbox('Navigate', ['Select an option', 'Go to Site 1', 'Go
 
 # 첫 번째 메뉴 선택시 Site 1로 리디렉션
 if page == 'Go to Site 1':
-    js = "window.location.href = 'https://cheokcheok101py-3dfsncgsxm7qqz8tmaqt8y.streamlit.app/'"
-    st.components.v1.html(f"<script>{js}</script>", height=0)
+    st.write("Redirecting to Site 1...")
+    st.markdown(f"<meta http-equiv='refresh' content='0; url=https://cheokcheok101py-3dfsncgsxm7qqz8tmaqt8y.streamlit.app/'>", unsafe_allow_html=True)
 
 # 두 번째 메뉴 선택시 Site 2로 리디렉션
 elif page == 'Go to Site 2':
-    js = "window.location.href = 'https://cheokcheok102py-ubq3swbcmet64wqnmtzvhp.streamlit.app/'"
-    st.components.v1.html(f"<script>{js}</script>", height=0)
+    st.write("Redirecting to Site 2...")
+    st.markdown(f"<meta http-equiv='refresh' content='0; url=https://cheokcheok102py-ubq3swbcmet64wqnmtzvhp.streamlit.app/'>", unsafe_allow_html=True)
