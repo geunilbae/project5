7dca03673f62c372787cbbc461add68904f9e94b
ksy277 <tkddud4514@gmail.com>
2024-10-18 16:38:40 +0900

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -36,10 +36,11 @@ if page == '프로젝트 및 팀원소개':
     # 팀원 소개 섹션
     st.markdown("<hr style='border-top:1px solid #dddddd;'>", unsafe_allow_html=True)
     st.markdown("## 팀원 소개")
-    
-    # 2x2 레이아웃으로 팀원 4명 소개
-    col1, col2 = st.columns(2)
 
+    # 1x4 레이아웃으로 팀원 4명 소개
+    col1, col2, col3, col4 = st.columns(4)
+
+    # 팀원 1
     with col1:
 #        st.image("team_member1.jpg", width=150)  # 팀원 1의 이미지 경로
         st.markdown("### 팀원 1")
@@ -48,6 +49,8 @@ if page == '프로젝트 및 팀원소개':
             **소개**: 프로젝트 전반을 총괄하고 팀을 이끌어가는 역할을 담당합니다.
         """)
 
+    # 팀원 2
+    with col2:
 #        st.image("team_member2.jpg", width=150)  # 팀원 2의 이미지 경로
         st.markdown("### 팀원 2")
         st.markdown("""
@@ -55,7 +58,8 @@ if page == '프로젝트 및 팀원소개':
             **소개**: 서버 및 데이터베이스 관리와 API 개발을 담당합니다.
         """)
 
-    with col2:
+    # 팀원 3
+    with col3:
 #        st.image("team_member3.jpg", width=150)  # 팀원 3의 이미지 경로
         st.markdown("### 팀원 3")
         st.markdown("""
@@ -63,12 +67,15 @@ if page == '프로젝트 및 팀원소개':
             **소개**: 사용자 인터페이스와 사용자 경험 설계를 담당합니다.
         """)
 
+    # 팀원 4
+    with col4:
 #        st.image("team_member4.jpg", width=150)  # 팀원 4의 이미지 경로
         st.markdown("### 팀원 4")
         st.markdown("""
             **역할**: 데이터 분석가  
             **소개**: 데이터 분석 및 머신러닝 모델 개발을 담당합니다.
         """)
+
 
 
 # Page 1 선택 시 'cheokcheok1_01.py' 실행
