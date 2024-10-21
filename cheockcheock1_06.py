046e7f35f84d3617dfa754fc5cbd602109923729
soryhon <soryhon2@gmail.com>
2024-10-21 23:50:32 +0900

    Update cheockcheock1_06.py

--- cheockcheock1_06.py
+++ cheockcheock1_06.py
@@ -1,14 +1,8 @@
 import streamlit as st
 import pandas as pd
 import os
-
 from datetime import datetime
-
 import backend as bd
-
-
-
-
 
 # Frontend 기능 구현 시작 ---
 
@@ -25,7 +19,7 @@ with col1:
     )
 with col2:
     st.markdown(
-        "<div style='text-align:right;width:100%;'><p style='font-size:15px; font-weight:normal; color:#aaaaaa;margin-top:10px;'>by <b style='font-size:16px;color:#0099FF'>CheokCeock</b><b style='font-size:22px;color:#009999'>1</b> <b style='font-size:14px;'>prototype v.01</b></p></div>",
+        "<div style='text-align:right;width:100%;'><p style='font-size:15px; font-weight:normal; color:#aaaaaa;margin-top:10px;'>by <b style='font-size:16px;color:#0099FF'>CheockCheock</b><b style='font-size:22px;color:#009999'>1</b> <b style='font-size:14px;'>prototype v.01</b></p></div>",
         unsafe_allow_html=True
     )
 
@@ -44,9 +38,15 @@ if github_info_loaded:
         st.markdown(
             "<hr style='border-top:1px solid #dddddd;border-bottom:0px solid #dddddd;width:100%;padding:0px;margin:0px'></hr>",
             unsafe_allow_html=True
-        )    
-        # 별점 선택 (슬라이더 사용)
-        score = st.slider("별점 선택 (1~5, 0.5 단위):", 0.5, 5.0, 1.0)
+        ) 
+        col1, col2, col3 = st.columns([0.07, 0.82, 0.11])
+        with col1:
+            st.write("")
+        with col2:
+            # 별점 선택 (슬라이더 사용)
+            score = st.slider("별점 선택 (1~5, 0.5 단위):", 1.0, 5.0, 1.0)
+        with col3:
+            st.write("")   
         # score에 따라 이미지 설정
         star_images = bd.get_star_images(score)
         
@@ -55,15 +55,15 @@ if github_info_loaded:
         
         # 각 열에 맞는 별 이미지 출력
         with col1:  # score 1점 별
-            st.image(star_images[0], width=100)
+            st.image(star_images[0], width=95)
         with col2:  # score 2점 별
-            st.image(star_images[1], width=100)
+            st.image(star_images[1], width=95)
         with col3:  # score 3점 별
-            st.image(star_images[2], width=100)
+            st.image(star_images[2], width=95)
         with col4:  # score 4점 별
-            st.image(star_images[3], width=100)
+            st.image(star_images[3], width=95)
         with col5:  # score 5점 별
-            st.image(star_images[4], width=100)
+            st.image(star_images[4], width=95)
    
 
     col1, col2, col3 = st.columns([0.2, 0.6, 0.2])
